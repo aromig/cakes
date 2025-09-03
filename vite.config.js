84e9f54f8a82e2a255faa8e6acfafe185c3baf81
aromig <adam.romig@gmail.com>
2025-09-03 12:07:24 -0400

    Migrate from vanilla vue2 to vite w/ vue2

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { createVuePlugin as vue } from 'vite-plugin-vue2'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    }
+})
