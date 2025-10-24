36d0751fa25259e389a6381bb51624ec048a900d
aromig <adam.romig@gmail.com>
2025-10-23 23:20:53 -0400

    updated to vite 7.1.12

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin as vue } from 'vite-plugin-vue2'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue2 from "@vitejs/plugin-vue2";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-    plugins: [
-        vue()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-    }
-})
+  plugins: [vue2()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
+});
